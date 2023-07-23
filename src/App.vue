16428e3822c5bf69fa4833d385221ae9b4ab1a7c
jahongir <jahongir0403@gmail.com>
2023-07-23 19:43:24 +0500

    portfolio

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wrapper">
+  <Navbar />
+  
+ 
+  
+  <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+import Navbar from './components/Navbar/Navbar.vue';
+
+
+</script>
+
---
+++ src/pages/Contacts.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="contacts">
+        <span class="me__content-text">Можете позвонить по телефону: <a class="me__content-text a" href="tel:+998935585925">+998935585925</a> <img src="../assets/images/phone.png" alt=""></span>
+        <span class="me__content-text">Telegram: <a class="me__content-text a" href="https://t.me/showtimejoha">@showtimejoha</a><img src="../assets/images/tg.png" alt=""></span>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
